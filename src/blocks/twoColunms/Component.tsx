9cd27e9ac045c0113bb48585fccf2f920d171323
Alfredo Izquierdo <alfredoizquierdo@Alfredos-MacBook-Pro-2.local>
2025-01-09 19:58:40 -0600

    fixed some ui issues

--- src/blocks/twoColunms/Component.tsx
+++ src/blocks/twoColunms/Component.tsx
@@ -7,19 +7,30 @@ import { SerializedLexicalNode } from 'node_modules/lexical/LexicalNode'
 const TwoColunmsBlock = ({
   content,
   image,
+  position,
 }: {
   content: SerializedEditorState<SerializedLexicalNode>
   image: Media
+  position: string
 }) => {
-  const isReverse = 'reverse'
-
+  const isReverse = position === 'reverse' ? true : false
   return (
-    <div className="flex flex-col md:flex-row items-center gap-6">
+    <div className="flex flex-col md:flex-row items-center gap-3">
       {/* Contenedor de la imagen */}
       <div
         className={`w-full md:w-1/2 ${isReverse ? 'order-last md:order-last' : 'order-first md:order-first'}`}
       >
-        {image && <Image src={`${image?.url}`} width={300} height={300} alt="column content" />}
+        <div className="relative w-full mx-auto h-96 md:h-96">
+          {image && (
+            <Image
+              src={`${image?.url}`}
+              fill
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              style={{ objectFit: 'contain' }}
+              alt="image"
+            />
+          )}
+        </div>
       </div>
 
       {/* Contenedor del texto */}
